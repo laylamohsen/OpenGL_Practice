1bb0bfc02b31e6e287654bfee02ffcf057076fef
laylamohsen <78423671+laylamohsen@users.noreply.github.com>
2024-06-25 10:26:27 -0400

    Textures

--- Resources/Shaders/default.frag
+++ Resources/Shaders/default.frag
@@ -1,10 +1,12 @@
-/* FRAGMENT SHADER*/
-
 #version 330 core
+
 out vec4 FragColor;
-in vec3 color;
+
+in vec2 texCoord;
+
+uniform sampler2D tex0;
+
 void main()
 {
-    FragColor = vec4(color, 0.8f);
+    FragColor = texture(tex0, texCoord);
 }
-
--- main.cpp
+++ main.cpp
@@ -1,30 +1,35 @@
-typedef void* GLeglImageOES;
 
-// MAKE SURE GLEW is included before any OpenGL headers!!
+#include <stb_image.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
 #include "shaderClass.h"
 #include "VBO.hpp"
 #include "VAO.hpp"
 #include "EBO.hpp"
-#include <stb_image.h>
+#include"Texture.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+bool fileExists(const std::string& filePath) {
+    std::ifstream file(filePath);
+    return file.good();
+}
 
 GLfloat vertices[] = {
-    // POSITIONS          //COLORS
-    -0.5f, -0.5f, 0.0f,     137.0/255.0f, 85.0/255.0f, 112.0/255.0f,   // outer left
-    0.5f, -0.5f, 0.0f,      226.0/255.0f, 158.0/255.0f, 255.0/255.0f,     // outer right
-    0.0f,  0.5f, 0.0f,      248.0/255.0f, 248.0/255.0f, 158.0/255.0f,      // outer up
-    -0.25f, 0.0f, 0.0f,     137.0/255.0f, 85/255.0f, 112.0/255.0f,   // inner left
-    0.25f, 0.0f, 0.0f,      137.0/255.0f, 85/255.0f, 112.0/255.0f,    // inner right
-    0.0f, -0.5f, 0.0f,      137.0/255.0f, 85/255.0f, 112.0/255.0f,    // inner down
+    // POSITIONS          // COLORS                        // TEX COORDS
+    -0.5f, -0.5f, 0.0f,   137.0f/255.0f, 85.0f/255.0f, 112.0f/255.0f,  0.0f, 0.0f,
+     0.5f, -0.5f, 0.0f,   226.0f/255.0f, 158.0f/255.0f, 255.0f/255.0f, 1.0f, 0.0f,
+    -0.5f,  0.5f, 0.0f,   248.0f/255.0f, 248.0f/255.0f, 158.0f/255.0f, 0.0f, 1.0f,
+     0.5f,  0.5f, 0.0f,   137.0f/255.0f, 85.0f/255.0f, 112.0f/255.0f,  1.0f, 1.0f,
 };
 
-// USED FOR PRIMITIVES -- THIS IS THE ORDER IN WHICH TRIANGLES ARE DRAWN AND THIS AVOIDS DUPLICATE CO-ORDINATES
 GLuint indices[] = {
-    0, 3, 5, // Lower left triangle
-    3, 2, 4, // Lower right triangle
-    5, 4, 1  // Upper triangle
+    0, 1, 2,
+    1, 3, 2
 };
 
 int main()
@@ -43,23 +48,19 @@ int main()
 
     // Create a GLFW window object of 800 by 800 pixels, naming it "OpenGL"
     GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL", NULL, NULL);
-    glViewport(0, 0, 800, 800);
-
-    
     if (window == NULL) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-    // Introduce the window into the current context
     glfwMakeContextCurrent(window);
+    glViewport(0, 0, 800, 800);
 
     // Load GLEW to configure OpenGL
     if (glewInit() != GLEW_OK) {
         std::cerr << "Failed to initialize GLEW" << std::endl;
         return -1;
     }
-
 
     // Generates Shader object using shaders default.vert and default.frag
     Shader shaderProgram("/Users/layla/Desktop/OpenGL_Tutorial/OpenGL_Tutorial/Resources/Shaders/default.vert", "/Users/layla/Desktop/OpenGL_Tutorial/OpenGL_Tutorial/Resources/Shaders/default.frag");
@@ -74,16 +75,58 @@ int main()
     EBO EBO1(indices, sizeof(indices));
 
     // Links VBO to VAO
-    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6*sizeof(float), (void*)(0*sizeof(float)));
-    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6*sizeof(float), (void*)(3*sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)(0 * sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+
     // Unbind all to prevent accidentally modifying them
     VAO1.Unbind();
     VBO1.Unbind();
     EBO1.Unbind();
-    
-    GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
-    
 
+    int widthImg, heightImg, numColCh;
+//    stbi_set_flip_vertically_on_load(true);
+    std::string texturePath = "/Users/layla/Desktop/OpenGL_Tutorial/OpenGL_Tutorial/Resources/Textures/buckooka.png";
+    if (!fileExists(texturePath)) {
+        std::cerr << "Texture file does not exist at path: " << texturePath << std::endl;
+        return -1;
+    }
+
+    std::cout << "Loading texture from: " << texturePath << std::endl;
+    unsigned char* bytes = stbi_load(texturePath.c_str(), &widthImg, &heightImg, &numColCh, 0);
+
+    if (!bytes) {
+        std::cerr << "Failed to load texture: " << stbi_failure_reason() << std::endl;
+        return -1;
+    } else {
+        std::cout << "Loaded texture: " << widthImg << "x" << heightImg << " with " << numColCh << " channels" << std::endl;
+    }
+    // Texture
+Texture bucky(("/Users/layla/Desktop/OpenGL_Tutorial/OpenGL_Tutorial/Resources/Textures/buckooka.png"), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
+    
+    bucky.texUnit(shaderProgram, "tex0", 0);
+//    GLuint texture;
+//    glGenTextures(1, &texture);
+//    glActiveTexture(GL_TEXTURE0);
+//    glBindTexture(GL_TEXTURE_2D, texture);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+//    if (numColCh == 4) {
+//        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
+//    } else if (numColCh == 3) {
+//        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthImg, heightImg, 0, GL_RGB, GL_UNSIGNED_BYTE, bytes);
+//    } else {
+//        std::cerr << "Unknown number of color channels in texture" << std::endl;
+//        stbi_image_free(bytes);
+//        return -1;
+//    }
+//    glGenerateMipmap(GL_TEXTURE_2D);
+//    stbi_image_free(bytes);
+//    glBindTexture(GL_TEXTURE_2D, 0);
+//    GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+    GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
     // Main while loop
     while (!glfwWindowShouldClose(window)) {
         // Specify the color of the background
@@ -93,10 +136,14 @@ int main()
         // Tell OpenGL which Shader Program we want to use
         shaderProgram.Activate();
         glUniform1f(uniID, 0.5f);
+        bucky.Bind();
+        /*glUniform1i(tex0Uni, 0);*/ // Ensure the uniform is set to the correct texture unit
+        
+//        glBindTexture(GL_TEXTURE_2D, texture);
         // Bind the VAO so OpenGL knows to use it
         VAO1.Bind();
         // Draw primitives, number of indices, datatype of indices, index of indices
-        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         // Swap the back buffer with the front buffer
         glfwSwapBuffers(window);
         // Take care of all GLFW events
@@ -108,6 +155,9 @@ int main()
     VBO1.Delete();
     EBO1.Delete();
     shaderProgram.Delete();
+    bucky.Delete();
+    
+//    glDeleteTextures(1, &texture);
     // Delete window before ending the program
     glfwDestroyWindow(window);
     // Terminate GLFW before ending the program
